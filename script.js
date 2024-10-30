a8d2e2809dbc40b5c6a6c77d06c5e0b6fe4ce2a9
HayzaHyaga <wallacepereira656@gmail.com>
2024-10-30 10:33:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const sites = [
+    "https://tmr2.intelbras.com.br:8443/",
+    "https://tmr3.intelbras.com.br:8443/"
+];
+
+let indice = 0;
+let timer;
+
+function mudarSite() {
+    const siteFrame = document.getElementById('siteFrame');
+    siteFrame.src = sites[indice];
+
+    const siteNumber = document.getElementById('siteNumber');
+    siteNumber.textContent = `TMR ${indice + 2}`;
+}
+
+function proximoSite() {
+    indice = (indice + 1) % sites.length;
+    mudarSite();
+}
+
+function anteriorSite() {
+    indice = (indice - 1 + sites.length) % sites.length;
+    mudarSite();
+}
+
+function iniciarTimer() {
+    timer = setInterval(proximoSite, 25000);
+}
+
+function toggleTimer() {
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+    } else {
+        iniciarTimer();
+    }
+}
+
+document.getElementById('nextButton').onclick = proximoSite;
+document.getElementById('prevButton').onclick = anteriorSite;
+document.getElementById('toggleButton').onclick = toggleTimer;
+
+mudarSite();
+iniciarTimer();
